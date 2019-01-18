2327a16d55db7b7f2a2b0cc391959bdb22287fea
Kirill Chernakov <ya@kirys.ru>
2019-01-18 05:02:09 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,213 +1,228 @@
 'use strict';
 
-const
-  gulp = require('gulp'),
-	util = require('gulp-util'),
-	// fs = require('fs'),
-	// path = require('path'),
-	// rename = require('gulp-rename'),
-	concat = require('gulp-concat'),
-	plumber = require('gulp-plumber'),
-	plumberErrorHandler = require('gulp-plumber-error-handler'),
-	webpack = require('webpack'),
-	webpackStream = require('webpack-stream'),
+const gulp = require('gulp'),
+  util = require('gulp-util'),
+  fs = require('fs'),
+  rename = require('gulp-rename'),
+  concat = require('gulp-concat'),
+  plumber = require('gulp-plumber'),
+  plumberErrorHandler = require('gulp-plumber-error-handler'),
+  webpack = require('webpack'),
+  webpackStream = require('webpack-stream'),
   postcss = require('gulp-postcss'),
-	csso = require('gulp-csso'),  
+  csso = require('gulp-csso'),
   uglify = require('gulp-uglify'),
   imagemin = require('gulp-imagemin'),
   pngquant = require('imagemin-pngquant'),
   changed = require('gulp-changed'),
-  svgSprite = require("gulp-svg-sprites");
+  svgSprite = require('gulp-svg-sprites'),
+  webpackConfig = require('./config/webpack.config'),
+  browserSync = require('browser-sync');
 
-// Review
-const 
-	env = util.env,
-	PRODUCTION = !!env.production; // --production
+const env = util.env,
+  PROXY = env.proxy || false, // --proxy=domain.local
+  PRODUCTION = !!env.production, // --production
+  BUILD_SCRIPTS_INITIALLY = env['inital-scripts']; // --initial-scripts
 
-const atSrc = (path) => `./src/${path}`;
-const atBuild = (path) => `./build/${path}`;
+function clean(...list) {
+  return [].concat(...list.filter(Boolean));
+}
 
+const SRC_DIR = './source';
 const src = {
-  styles: atSrc('styles'),
-  scripts: atSrc('scripts'),
-  images: atSrc('images'),
-  fonts: atSrc('fonts'),
-  icons: atSrc('icons'),
+  base: SRC_DIR,
+  styles: SRC_DIR + '/styles',
+  scripts: SRC_DIR + '/scripts',
+  images: SRC_DIR + '/images',
+  fonts: SRC_DIR + '/fonts',
+  icons: SRC_DIR + '/icons',
+  svg: SRC_DIR + '/svg'
 };
 
+const BUILD_DIR = './';
 const build = {
-  styles: atBuild('css'),
-  scripts: atBuild('js'),
-  images: atBuild('img'),
-  fonts: atBuild('fonts'),
-  icons: atBuild('icons')
+  base: BUILD_DIR,
+  styles: BUILD_DIR + 'css',
+  scripts: BUILD_DIR + 'js',
+  images: BUILD_DIR + 'img',
+  fonts: BUILD_DIR + 'fonts',
+  icons: BUILD_DIR + 'icons',
+  svg: BUILD_DIR + 'svg'
 };
 
-function errorHandler(taskName) {
-	return {errorHandler: plumberErrorHandler(`${taskName} build failed!`)};
+function __errorHandler(taskName) {
+  return { errorHandler: plumberErrorHandler(`${taskName} build failed!`) };
+}
+
+function startBS() {
+  if (!PROXY) return;
+  browserSync.init({
+    notify: false,
+    proxy: PROXY,
+    serveStatic: [
+      {
+        route: '/static',
+        dir: './'
+      }
+    ]
+  });
 }
 
 const postcssProcessors = [
   require('postcss-import')(),
   require('postcss-mixins')(),
-  require('postcss-simple-vars')(),  
-  require("postcss-cssnext")(),
-  require("postcss-nested")(),
-  require('postcss-inline-svg'),  
+  require('postcss-cssnext')(),
+  require('postcss-nested')(),
+  require('postcss-inline-svg')
 ];
 
-// Build styles
-gulp.task('styles', function () {
-  return gulp.src(`${src.styles}/` + '*.css')
-    .pipe(plumber(errorHandler('Styles')))  
-    .pipe(postcss(postcssProcessors))
-    // .pipe(rename(build.styles))
-    .pipe(PRODUCTION ? csso({debug: true}) : util.noop())
-    .pipe(gulp.dest(build.styles));
+var styleTasks = [];
+fs.readdirSync(src.styles).map(function(filename) {
+  var taskName = filename.out.split('.')[0];
+  styleTasks.push(taskName);
+  gulp.task(taskName, function() {
+    return (
+      gulp
+        .src(`${src.styles}/${filename}`)
+        // TODO: changed
+        .pipe(plumber(__errorHandler(`Styles (${taskName})`)))
+        .pipe(postcss(postcssProcessors))
+        .pipe(rename(filename))
+        .pipe(PRODUCTION ? csso({ debug: true }) : util.noop())
+        .pipe(gulp.dest(build.styles))
+        .pipe(browserSync.stream())
+    );
+  });
 });
 
-// Copy vendor styles
+// Copy styles vendors
 gulp.task('copy-vendor-styles', () => {
-	return gulp
-		.src(`${src.styles}/vendors/*`)
-		.pipe(gulp.dest(build.styles));
+  return gulp.src(`${src.styles}/vendors/*`).pipe(gulp.dest(build.styles));
 });
 
-// Uglify and copy vendor scripts
+// Build for serve
+gulp.task('build', function() {
+  gulp.start.apply(
+    this,
+    [].concat(
+      'opt-images',
+      'opt-svg',
+      'sprites',
+      'copy-vendor-styles',
+      styleTasks,
+      'scripts-deps',
+      'scripts-deps-legacy',
+      'scripts-main'
+    )
+  );
+});
+
+// Run live
+gulp.task('default', function() {
+  gulp.start.apply(
+    this,
+    clean(
+      'opt-images',
+      'opt-svg',
+      'sprites',
+      'copy-vendor-styles',
+      BUILD_SCRIPTS_INITIALLY && 'scripts-deps',
+      BUILD_SCRIPTS_INITIALLY && 'scripts-deps-legacy',
+      BUILD_SCRIPTS_INITIALLY && 'scripts-main',
+      styleTasks,
+      ['watch']
+    )
+  );
+});
+
+// Федеральная служба по контролю за оборотом файлов
+gulp.task('watch', function() {
+  startBS();
+  gulp.watch(src.styles + '/**/*.css', styleTasks);
+  gulp.watch(`${src.styles}/vendors` + '/**/*.css', ['copy-vendors-styles']);
+  gulp.watch(src.scripts + '/**/*.js', ['scripts-main']);
+  gulp.watch(src.scripts + '/vendors/**/*.js', [
+    'scripts-deps',
+    'scripts-deps-legacy'
+  ]);
+  gulp.watch(src.images + '/**/*.{png,svg,jpg,jpeg,gif, ico}', ['opt-images']);
+  gulp.watch(src.svg + '/**/*.svg', ['opt-svg']);
+  gulp.watch(src.images + '/**/*.svg', ['sprites']);
+  gulp.watch(['../templates/**/*']).on('change', browserSync.reload);
+});
+
+gulp.task('watch-js', function() {
+  startBS();
+  gulp.watch(src.scripts + '/**/*.js', ['scripts-main']);
+});
+
 gulp.task('scripts-deps', function() {
-  return  gulp.src(`${src.scripts}/vendors/**/*.js`)
-    .pipe(plumber(errorHandler('Dependencies scripts')))
+  return gulp
+    .src(`${src.scripts}/vendors/*.js`)
+    .pipe(plumber(__errorHandler('Dependencies scripts')))
     .pipe(concat(`vendors.js`))
     .pipe(uglify())
-    .pipe(gulp.dest(build.scripts));
+    .pipe(gulp.dest(build.scripts))
+    .pipe(browserSync.stream())
 });
 
-let webpackConfig = {
-	entry: {
-    // TODO: dynamic
-    index: `${src.scripts}/index.js`,
-  },
-	output: {
-		filename: '[name].js',
-	},
-	module: {
-		rules: [{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			use: [{
-				loader: 'babel-loader',
-				options: {
-          presets: ['es2015'],
-          cacheDirectory: true
-        }
-			}],
-		}],
-	},
-	devtool: 'source-map',
-	plugins: [
-		new webpack.optimize.OccurrenceOrderPlugin()
-	]
-};
-
-let webpackUglify = new webpack.optimize.UglifyJsPlugin({
-	compress: {
-		warnings: false,
-		screw_ie8: true,
-		conditionals: true,
-		unused: true,
-		comparisons: true,
-		sequences: true,
-		dead_code: true,
-		evaluate: true,
-		join_vars: true,
-		if_return: true,
-	},
-	output: {
-		comments: false
-	},
-	sourceMap: true
-});
-webpackConfig.plugins.push(
-  new webpack.DefinePlugin({
-    PRODUCTION: JSON.stringify(Boolean(PRODUCTION))
-  })
-);
-if (PRODUCTION) {
-	webpackConfig.plugins.push(webpackUglify);
-	webpackConfig.devtool = '';
-}
-
-// Build scripts
 gulp.task('scripts-main', () => {
   return webpackStream(webpackConfig, webpack)
-    .pipe(plumber(errorHandler('Main scripts')))
-    .pipe(gulp.dest(build.scripts));
+    .pipe(plumber(__errorHandler('Main scripts')))
+    .pipe(gulp.dest(build.scripts))
+    .pipe(browserSync.stream());
 });
 
-// Optimize images
+// Make images to weight less TODO: tinypng or something like that
 gulp.task('opt-images', () => {
-	return gulp
-		.src(src.images + '/**/*.{png,svg,jpg,jpeg,gif,ico}')
-		.pipe(changed(build.images))
-    .pipe(plumber(errorHandler('Opt images')))
-		.pipe(imagemin({
-			progressive: true,
-			svgoPlugins: [{removeViewBox: false}],
-      use: [pngquant()],
-      // imagemin.svgo({
-      //   plugins: [
-      //     {removeViewBox: false},
-      //     {cleanupIDs: false}
-      //   ]
-      // })
-		}))
-		.pipe(gulp.dest(build.images));
+  return gulp
+    .src(src.images + '/**/*.{png,svg,jpg,jpeg,gif,ico}')
+    .pipe(changed(build.images))
+    .pipe(plumber(__errorHandler('Opt images')))
+    .pipe(
+      imagemin({
+        progressive: true,
+        svgoPlugins: [{ removeViewBox: false }],
+        use: [pngquant()]
+      })
+    )
+    .pipe(gulp.dest(build.images));
 });
 
-// Build svg sprite
-gulp.task('icons', function () {
-  return gulp.src(src.icons + '/**/*.svg')
-      .pipe(plumber(errorHandler('SVG icons')))
-      .pipe(svgSprite({
-        mode: "symbols", 
+gulp.task('opt-svg', () => {
+  return (
+    gulp
+      .src(src.svg + '/**/*.svg')
+      .pipe(plumber(__errorHandler('Opt svg')))
+      .pipe(
+        imagemin([
+          imagemin.svgo({
+            plugins: [
+              { removeViewBox: false },
+              { cleanupIDs: false },
+              { convertStyleToAttrs: false },
+              { removeDimensions: false },
+              { removeUselessDefs: false }
+            ]
+          })
+        ])
+      )
+      .pipe(gulp.dest(build.svg))
+  );
+});
+
+gulp.task('sprites', function() {
+  return gulp
+    .src(src.icons + '/**/*.svg')
+    .pipe(plumber(__errorHandler('SVG sprites')))
+    .pipe(
+      svgSprite({
+        mode: 'symbols',
         preview: false,
         svg: {
           symbols: 'symbols.svg'
         }
-      }))
-      .pipe(gulp.dest(build.icons));
-});
-
-// Watching
-gulp.task('watch', function() {
-  gulp.watch(src.styles + '/**/*.css', 'styles');
-  gulp.watch(`${src.styles}/vendors` + '/**/*.css', ['copy-vendors-styles']);  
-  gulp.watch(src.scripts + '/**/*.js', ['scripts-main']);
-  gulp.watch(src.scripts + '/vendors/**/*.js', ['scripts-deps']);  
-  gulp.watch(src.images + '/**/*.{png,svg,jpg,jpeg,gif, ico,svg}', ['opt-images']); 
-  gulp.watch(src.images + '/**/*.svg', ['icons']);     
-});
-
-const defaultTasks = [
-  'opt-images', 
-  'icons',
-  'copy-vendor-styles',
-  'styles', 
-  'scripts-deps',
-  'scripts-main',
-];
-
-// Build
-gulp.task('default', function() {
-  gulp.start.apply(this, defaultTasks);
-});
-
-// Build with watch
-gulp.task('live', function() {
-  gulp.start.apply(this,
-    [].concat(
-      defaultTasks,
-      ['watch']
+      })
     )
-  );
+    .pipe(gulp.dest(build.icons));
 });
